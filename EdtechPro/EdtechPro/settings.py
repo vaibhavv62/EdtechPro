c16d0e9c0b3bf0f2cdfbe5b457e24a7290ace179
Vaibhav <kvaibhav1998@gmail.com>
2021-11-16 16:46:43 +0530

    Allowed Host PythonAnyWhere

--- EdtechPro/EdtechPro/settings.py
+++ EdtechPro/EdtechPro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1qb-ub)!0yp52h@nc*l**7j5s@3q45h)ly77nk8n9gp(r2d^cx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vaibhavv62.pythonanywhere.com']
 
 
 # Application definition
